111c8a114500add3a8a18be0fb108233103fd7ea
dhanazam <ramadanazam93@gmail.com>
2024-08-16 13:38:34 +0800

    add initial screen

---
+++ lib/src/presentation/screens/library_screen/index.dart
@@ -0,0 +1,1 @@
+export 'library_screen.dart';
---
+++ lib/src/presentation/screens/library_screen/library_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LibraryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Library'),
+      ),
+      body: Center(
+        child: Text('Library Screen'),
+      ),
+    );
+  }
+}
