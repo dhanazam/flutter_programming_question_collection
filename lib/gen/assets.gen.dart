dec4953dd397800c31f8fd2be6039e6df50aad6c
Ramadhan <ramadanazam93@gmail.com>
2024-08-21 11:07:24 +0800

    add generative

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIntroductionGen {
+  const $AssetsIntroductionGen();
+
+  /// File path: assets/introduction/books.svg
+  String get books => 'assets/introduction/books.svg';
+
+  /// File path: assets/introduction/questions.svg
+  String get questions => 'assets/introduction/questions.svg';
+
+  /// List of all assets
+  List<String> get values => [books, questions];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIntroductionGen introduction = $AssetsIntroductionGen();
+}
